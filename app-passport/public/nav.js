c59f24a2dfd08d79b4d6ddbce7e30d7abc31a313
stevencueto <steven.cueto1305@gmail.com>
2022-03-16 13:22:39 -0400

    deleted app passport

--- app-passport/public/nav.js
+++
@@ -1,8 +0,0 @@
-//hamburger
-const hamburger = document.querySelector(".hamburger");
-const topNav = document.querySelector("#nav-ul");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    topNav.classList.toggle("active");
-})
-document.querySelectorAll(".nav-links").forEach(n => n.addEventListener)
