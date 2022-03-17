6cbdc3ef6545f3caf1200f13b08ea17a9c3fa306
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-03-17 11:46:10 -0600

    heroku

---
+++ public/js/carousel.js
@@ -0,0 +1,38 @@
+//carousel slideshow code
+let slidePosition = 0;
+const slides = document.getElementsByClassName('slide');
+const totalSlides = slides.length;
+const next = document.getElementById('next').addEventListener("click", () => {
+    moveToNextSlide();
+})
+document.getElementById('prev').addEventListener("click", () => {
+    moveToPrevSlide();
+})
+const updateSlidePosition = () => {
+    for (let slide of slides) {
+        console.log(slide);
+        slide.classList.remove('slide--visible');
+        slide.classList.add('slide--hidden');
+    }
+    slides[slidePosition].classList.remove('slide--hidden');
+    slides[slidePosition].classList.add('slide--visible');
+}
+const moveToNextSlide = () => {
+    updateSlidePosition();
+    if(slidePosition === totalSlides -1) {
+        slidePosition = 0;
+    }
+    else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+const moveToPrevSlide = () => {
+    updateSlidePosition();
+    if(slidePosition === 0) {
+        slidePosition = slides.length -1;
+    }
+    else {
+        slidePosition--;
+    }
+}
